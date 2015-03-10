992ec63a282f89db32c8570712c5b4843dbc24c5
xammi-1 <kmg.xammi.1@gmail.com>
2015-03-10 17:56:48 +0300

    Jetty minimal server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
 /**
  * Created by max on 10.03.15.
  */
-public class Main {
-    public static void main(String [] argv) {
-        System.out.println("Hello world");
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class Main extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8082);
+        server.setHandler(new Main());
+        server.start();
+        server.join();
     }
 }
