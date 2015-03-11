4b6a42e3806f716dc8d7bd80d0cb97a0a84218ba
NFetissow <nfetissow@gmail.com>
2015-03-11 15:31:11 +0300

    Created Logger interface and  empty implementation

---
+++ src/main/java/loggers/AwesomeLogger.java
@@ -0,0 +1,25 @@
+package loggers;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * This is The Logger
+ */
+public class AwesomeLogger implements Logger {
+    protected AwesomeLogger() {}
+
+    private String fileName;
+
+
+    @Override
+    public void log(String tag, String msg) {
+
+    }
+
+    @Override
+    public void log(String msg) {
+
+
+    }
+}
---
+++ src/main/java/loggers/Logger.java
@@ -0,0 +1,9 @@
+package loggers;
+
+/**
+ * Created by nfetissow on 3/11/15.
+ */
+public interface Logger {
+    public void log(String tag, String msg);
+    public void log(String msg);
+}
